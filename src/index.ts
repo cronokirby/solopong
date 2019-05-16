1b0fb5a67c01bfa970362a4f27891dc251a32c7d
Lúcás Meier <cronokirby@gmail.com>
2019-05-16 21:20:28 +0200

    Add basic paddle movement

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,61 @@
 import * as PIXI from 'pixi.js';
 
+const WIDTH = 600;
+const HEIGHT = 600;
+const CENTER = { x: WIDTH / 2, y: HEIGHT / 2 };
+const RADIUS = 240;
+
+
+interface Pos {
+    readonly x: number;
+    readonly y: number;
+}
 
 interface Circle {
-    readonly x: number,
-    readonly y: number,
-    readonly radius: number
+    readonly pos: Pos;
+    readonly radius: number;
 }
 
 function circle(stage: PIXI.Container, circle: Circle) {
     const graphics = new PIXI.Graphics();
     graphics.lineStyle(3, 0);
-    graphics.drawCircle(circle.x, circle.y, circle.radius);
+    graphics.drawCircle(circle.pos.x, circle.pos.y, circle.radius);
     stage.addChild(graphics);
 }
 
+
+class Paddle {
+    private static readonly WIDTH = 80;
+    private mouse: PIXI.interaction.InteractionData;
+    private graphics: PIXI.Graphics;
+
+    constructor(app: PIXI.Application) {
+        this.mouse = app.renderer.plugins.interaction.mouse;
+        this.graphics = new PIXI.Graphics();
+        this.graphics.beginFill(0x0000FF);
+        this.graphics.lineStyle(5, 0x0000FF);
+        this.graphics.drawRect(0, 0, Paddle.WIDTH, 10);
+        app.stage.addChild(this.graphics);
+    }
+
+    update(deltaS: number) {
+        const mousePos = this.mouse.global;
+        const dir = { x: mousePos.x - CENTER.x, y: mousePos.y - CENTER.y };
+        const dirScale = Math.sqrt(dir.x * dir.x + dir.y * dir.y);
+        const unitDir = { x: dir.x / dirScale, y: dir.y / dirScale };
+        const { x, y } = { x: unitDir.x * RADIUS + CENTER.x, y: unitDir.y * RADIUS + CENTER.y };
+        if (x >= 0) this.graphics.x = x;
+        if (y >= 0) this.graphics.y = y;
+        const piAngle = unitDir.y >= 0 ? Math.acos(unitDir.x) : -Math.acos(unitDir.x);
+        const angle = piAngle * 180 / Math.PI + 90;
+        this.graphics.angle = angle;
+    }
+}
+
+
 const app = new PIXI.Application({
-    width: 600,
-    height: 600,
+    width: WIDTH,
+    height: HEIGHT,
     resolution: devicePixelRatio,
     backgroundColor: 0xFFFFFF,
     antialias: true
@@ -29,10 +68,10 @@ window.addEventListener('load', () => {
     }
 })
 
-const graphics = new PIXI.Graphics();
-graphics.beginFill(0x0000FF);
-graphics.lineStyle(5, 0x0000FF);
-graphics.drawRect(20, 20, 60, 10);
-app.stage.addChild(graphics);
+circle(app.stage, { pos: CENTER, radius: RADIUS });
+const paddle = new Paddle(app);
 
-circle(app.stage, { x: 300, y: 300, radius: 240 });
+app.ticker.add(() => {
+    const deltaS = app.ticker.deltaMS / 1000;
+    paddle.update(deltaS);
+})
