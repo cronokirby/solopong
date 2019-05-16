b75efc76cff07db41f5895887ed0ae54dd3940c9
Lúcás Meier <cronokirby@gmail.com>
2019-05-16 20:09:50 +0200

    Add center circle

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import * as PIXI from 'pixi.js';
 
 
+interface Circle {
+    readonly x: number,
+    readonly y: number,
+    readonly radius: number
+}
+
+function circle(stage: PIXI.Container, circle: Circle) {
+    const graphics = new PIXI.Graphics();
+    graphics.lineStyle(3, 0);
+    graphics.drawCircle(circle.x, circle.y, circle.radius);
+    stage.addChild(graphics);
+}
+
 const app = new PIXI.Application({
-    width: 400,
-    height: 400,
+    width: 600,
+    height: 600,
     resolution: devicePixelRatio,
     backgroundColor: 0xFFFFFF,
     antialias: true
@@ -19,9 +32,7 @@ window.addEventListener('load', () => {
 const graphics = new PIXI.Graphics();
 graphics.beginFill(0x0000FF);
 graphics.lineStyle(5, 0x0000FF);
-graphics.drawRect(0, 0, 60, 10);
-graphics.x = 20;
-graphics.y = 20;
-graphics.visible = true;
-
+graphics.drawRect(20, 20, 60, 10);
 app.stage.addChild(graphics);
+
+circle(app.stage, { x: 300, y: 300, radius: 240 });
